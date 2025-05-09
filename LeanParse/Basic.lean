f47f58efcd93a8686fac322a522559f05c53050e
Per Lindgren <per.lindgren@ltu.se>
2025-05-09 17:45:25 +0200

    lit

--- LeanParse/Basic.lean
+++ LeanParse/Basic.lean
@@ -1,27 +1,38 @@
-import Init.System.IO
 import Std.Internal.Parsec
 
 open Std.Internal.Parsec
-open Std.Internal.Parsec.ByteArray
+open Std.Internal.Parsec.String
 
-def parseZero : Parser Unit := skipByteChar '0'
+inductive Terminal where
+  | int : Int    → Terminal
+  | id  : String → Terminal
+deriving Repr
+
+open Terminal
+@[inline]
+def parseIntLitTerm : Parser Terminal := do
+  return int (← digits)
+
+-- test IntLitTerm
+#eval (parseIntLitTerm.run "78") -- 78
+#eval (parseIntLitTerm.run "-3") -- expected digit
 
 @[inline]
-def parsePos : Parser Nat := do
-  let ident ← digits
-  if ident == 0 then
-    fail "id was 0"
-  else
-    return ident
+def parseHexLitTerm : Parser Terminal := do
+  let _ ← pstring "0x"
+  return int ((← many hexDigit).reverse.foldr toHex 0)
+  where toHex c acc : Int :=
+    acc * 16 +
+    if ('0' ≤ c ∧ c ≤ '9') then
+      c.toNat - '0'.toNat
+    else if ('a' ≤ c ∧ c ≤ 'f') then
+      10 + c.toNat - 'a'.toNat
+    else
+      10 + c.toNat - 'A'.toNat
 
-@[inline]
-def parseId : Parser Nat := parsePos
-
-def parseIdList : Parser (Array Nat) := do
-  many idWs
-where
-  @[inline]
-  idWs : Parser Nat := do
-    let ident ← attempt parseId
-    skipByteChar ' '
-    return ident
+-- test HexLitTerm
+#eval (parseHexLitTerm.run "0x7")
+#eval (parseHexLitTerm.run "0xc")
+#eval (parseHexLitTerm.run "0xF")
+#eval (parseHexLitTerm.run "0x10")
+#eval (parseHexLitTerm.run "x10") -- expected 0x
